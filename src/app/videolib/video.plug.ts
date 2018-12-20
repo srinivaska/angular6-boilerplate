545e63ee6213040c5aa0dd8d8acd9fbeb635e95d
Srinivas Kandukuri <srinivas.kandukuri@photoninfotech.net>
2018-12-20 17:09:57 +0530

    added playlist

--- src/app/videolib/video.plug.ts
+++ src/app/videolib/video.plug.ts
@@ -1,123 +1,123 @@
-component({
-    selector: 'app-video-player',
-    templateUrl: './video-player.component.html',
-    styleUrls: ['./video-player.component.css']
-  })
-  export class VideoPlayerComponent implements AfterViewInit, OnChanges, OnDestroy {
+// component({
+//     selector: 'app-video-player',
+//     templateUrl: './video-player.component.html',
+//     styleUrls: ['./video-player.component.css']
+//   })
+//   export class VideoPlayerComponent implements AfterViewInit, OnChanges, OnDestroy {
   
-@Input() selectedClip: any;
-@Output() nextClip = new EventEmitter<string>();
+// @Input() selectedClip: any;
+// @Output() nextClip = new EventEmitter<string>();
 
-videoPlayer;
-clipLoaded = false;
-isPlaying = false;
-muted = false;
-currentTime;
-selectedClipSource: string;
-clipDuration: string;
-captionsUrl: string;
+// videoPlayer;
+// clipLoaded = false;
+// isPlaying = false;
+// muted = false;
+// currentTime;
+// selectedClipSource: string;
+// clipDuration: string;
+// captionsUrl: string;
 
-constructor() { }
+// constructor() { }
 
-ngAfterViewInit() {
-  const options = {
-    hls: {
-      withCredentials: true
-    }
-  };
-  this.videoPlayer = videojs('my-player', {
-    flash: options, html5: options,
-    plugins: {
-      timeOffset: {}
-    }
-  }, (onPlayerReady) => {
-    videojs.log('VideoJs: The video player successfully initialized.');
-    this.videoPlayer.on('timeupdate', () => {
-      this.updateSeek();
-      this.updateCurrentTime();
-    });
-  });
-}
+// ngAfterViewInit() {
+//   const options = {
+//     hls: {
+//       withCredentials: true
+//     }
+//   };
+//   this.videoPlayer = videojs('my-player', {
+//     flash: options, html5: options,
+//     plugins: {
+//       timeOffset: {}
+//     }
+//   }, (onPlayerReady) => {
+//     videojs.log('VideoJs: The video player successfully initialized.');
+//     this.videoPlayer.on('timeupdate', () => {
+//       this.updateSeek();
+//       this.updateCurrentTime();
+//     });
+//   });
+// }
 
-ngOnChanges() {
-  if (typeof this.selectedClip !== 'undefined') {
-    this.videoPlayer.pause();
-    this.videoPlayer.currentTime(0);
-    this.loadVideoSource();
-  } else {
-    if (this.videoPlayer) {
-      this.resetVideo();
-      this.clipLoaded = false;
-    }
-  }
-}
+// ngOnChanges() {
+//   if (typeof this.selectedClip !== 'undefined') {
+//     this.videoPlayer.pause();
+//     this.videoPlayer.currentTime(0);
+//     this.loadVideoSource();
+//   } else {
+//     if (this.videoPlayer) {
+//       this.resetVideo();
+//       this.clipLoaded = false;
+//     }
+//   }
+// }
 
-loadVideoSource(): void {
-    this.setClipTimeOffset(this.selectedClip.streamingUrl);
-    this.clipLoaded = true;
-    this.videoPlayer.src({ type: 'application/vnd.apple.mpegurl', src: this.selectedClip.streamingUrl });
-    this.isPlaying = true;
-    this.videoPlayer.play();
-    this.clipDuration = this.selectedClip.duration;
-}
+// loadVideoSource(): void {
+//     this.setClipTimeOffset(this.selectedClip.streamingUrl);
+//     this.clipLoaded = true;
+//     this.videoPlayer.src({ type: 'application/vnd.apple.mpegurl', src: this.selectedClip.streamingUrl });
+//     this.isPlaying = true;
+//     this.videoPlayer.play();
+//     this.clipDuration = this.selectedClip.duration;
+// }
 
-toggleClosedCaptions() {
-  if (this.captionsUrl) {
-    this.captionsUrl = undefined;
-  } else {
-    this.captionsUrl = this.selectedClip['captions'];
-  }
-}
+// toggleClosedCaptions() {
+//   if (this.captionsUrl) {
+//     this.captionsUrl = undefined;
+//   } else {
+//     this.captionsUrl = this.selectedClip['captions'];
+//   }
+// }
 
-playVideo() {
-  this.videoPlayer.play();
-}
+// playVideo() {
+//   this.videoPlayer.play();
+// }
 
-pauseVideo() {
-  this.videoPlayer.pause();
-}
+// pauseVideo() {
+//   this.videoPlayer.pause();
+// }
 
-toggleMute() {
-  this.muted = !this.muted;
-}
+// toggleMute() {
+//   this.muted = !this.muted;
+// }
 
-enterFullscreen() {
-  this.videoPlayer.requestFullscreen();
-}
+// enterFullscreen() {
+//   this.videoPlayer.requestFullscreen();
+// }
 
-seek($event) {
-  if (this.clipLoaded) {
-    let goto;
-    goto = this.videoPlayer.duration() * ($event.target.value / 100);
-    this.videoPlayer.currentTime(goto);
-  }
-}
+// seek($event) {
+//   if (this.clipLoaded) {
+//     let goto;
+//     goto = this.videoPlayer.duration() * ($event.target.value / 100);
+//     this.videoPlayer.currentTime(goto);
+//   }
+// }
 
-getParameterByName(name, url) {
-  name = name.replace(/[\[\]]/g, '\\$&');
-  const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-    results = regex.exec(url);
-  if (!results) {
-    return null;
-  }
-  if (!results[2]) {
-    return '';
-  }
-  return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
+// getParameterByName(name, url) {
+//   name = name.replace(/[\[\]]/g, '\\$&');
+//   const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+//     results = regex.exec(url);
+//   if (!results) {
+//     return null;
+//   }
+//   if (!results[2]) {
+//     return '';
+//   }
+//   return decodeURIComponent(results[2].replace(/\+/g, ' '));
+// }
 
-setClipTimeOffset(streamingUrl: string): void {
-  const clipIn = Math.round(parseFloat(this.getParameterByName('clipin', streamingUrl)));
-  const clipOut = Math.round(parseFloat(this.getParameterByName('clipout', streamingUrl)));
-  this.videoPlayer.timeOffset({
-    start: clipIn,
-    end: clipOut,
-    restart_beginning: false
-  });
-}
+// setClipTimeOffset(streamingUrl: string): void {
+//   const clipIn = Math.round(parseFloat(this.getParameterByName('clipin', streamingUrl)));
+//   const clipOut = Math.round(parseFloat(this.getParameterByName('clipout', streamingUrl)));
+//   this.videoPlayer.timeOffset({
+//     start: clipIn,
+//     end: clipOut,
+//     restart_beginning: false
+//   });
+// }
 
-ngOnDestroy() {
-  if (this.videoPlayer) {
-    this.videoPlayer.dispose();
-  }
-}
+// ngOnDestroy() {
+//   if (this.videoPlayer) {
+//     this.videoPlayer.dispose();
+//   }
+// }
--- src/app/videolib/videolib.component.ts
+++ src/app/videolib/videolib.component.ts
@@ -1,6 +1,9 @@
 
 import { Component, AfterViewInit, Input } from '@angular/core';
 import videojs from 'video.js';
+import videojsplay from 'videojs-playlist';
+import chromecast from 'videojs-chromecast';
+
 
 
 @Component({
@@ -11,23 +14,69 @@ import videojs from 'video.js';
 export class VideolibComponent implements AfterViewInit {
  
   public vjs: videojs.Player;
+  public playlist: any;
   @Input() urlVideo: string;
   @Input() urlPoster: string;
 
-  constructor() {}
+  constructor() {
+    
+    this.playlist = [{
+      sources: [{
+        src: 'http://media.w3.org/2010/05/sintel/trailer.mp4',
+        type: 'video/mp4'
+      }],
+      poster: 'http://media.w3.org/2010/05/sintel/poster.png',
+      title: 'sample Title'
+    }, {
+      sources: [{
+        src: 'http://media.w3.org/2010/05/bunny/trailer.mp4',
+        type: 'video/mp4'
+      }],
+      poster: 'http://media.w3.org/2010/05/bunny/poster.png',
+      title: 'sample Title'
+    }, {
+      sources: [{
+        src: 'http://vjs.zencdn.net/v/oceans.mp4',
+        type: 'video/mp4'
+      }],
+      poster: 'http://media.w3.org/2010/05/video/poster.png',
+      title: 'sample Title'
+    }, {
+      sources: [{
+        src: 'http://media.w3.org/2010/05/bunny/movie.mp4',
+        type: 'video/mp4'
+      }],
+      poster: 'http://media.w3.org/2010/05/bunny/poster.png',
+      title: 'sample Title'
+    }, {
+      sources: [{
+        src: 'http://media.w3.org/2010/05/video/movie_300.mp4',
+        type: 'video/mp4'
+      }],
+      poster: 'http://media.w3.org/2010/05/video/poster.png',
+      title: 'sample Title'
+    }];
+  }
 
 
   ngAfterViewInit() {
-    const options = {
-      'sources' : [{
-        'src' : 'https://ia800209.us.archive.org/24/items/WildlifeSampleVideo/Wildlife.mp4',
-        'type' : 'video/mp4'
-        }
-      ],
-      'poster' : this.urlPoster
-    };
-    this.vjs = videojs('my-player', options);
+    videojsplay;
+    this.initVideoJs();
+  }
 
+
+  initVideoJs() {
+    console.log(chromecast);
+    this.vjs = videojs('my-player', {chromecast:{appId:undefined}});
+    this.vjs.playlist(this.playlist);
+    this.vjs.playlist.autoadvance(0);
+    console.log(this.vjs);
+  }
+
+
+  onClick(index) {
+    console.log(index);
+    this.vjs.playlist.currentItem(index);
   }
 }
 
